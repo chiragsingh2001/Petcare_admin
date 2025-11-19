ee13e6e6139b3ec1cd82a08fa6decfb1e26f113c
kshitize singh <chiragsingh9251632311@gmail.com>
2025-11-19 15:26:07 +0530

    User add code

--- tests/test_user.py
+++ tests/test_user.py
@@ -2,8 +2,11 @@ import pytest
 from pages.user_management import UserManagement
 from playwright.sync_api import Page, expect
 
-def test_visibility(page: Page):
+
+@pytest.mark.parametrize("name, email, phone, password ",[("hemanth", "hemanth@mailinator.com", "7225163231", "Admin@123")])
+def test_add_user(page: Page, name, email, phone, password):
     user = UserManagement(page)
     user.goto()
-    
+    user.add_user()
+    user.add_details(name, email, phone, password)
 
